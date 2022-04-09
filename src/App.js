c4e02e7aa165e8591137bc41e1658824a4d6a13e
Viktoriya Shpachynska <viktoriya.shpachynska@gmail.com>
2022-04-09 10:12:15 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+// import { DatePicker } from "antd";
+import "antd/dist/antd.css";
+
+function App() {
+  return <div className="App">{/* <DatePicker /> */}</div>;
+}
+
+export default App;

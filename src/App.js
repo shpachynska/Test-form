c1cb29d452f2b2c5753a10b31d92e49eecac2110
Viktoriya Shpachynska <viktoriya.shpachynska@gmail.com>
2022-04-11 00:37:49 +0200

    second version

--- src/App.js
+++ src/App.js
@@ -1,102 +1,12 @@
 import "./App.css";
-import "antd/dist/antd.css";
-import {
-  useForm,
-  FormProvider,
-  useFormContext,
-  Controller,
-} from "react-hook-form";
-import { Button, DatePicker, Select, Input, Dropdown } from "antd";
-import { useState } from "react";
 import React from "react";
-import MyInput from "./components/Input/Input";
+import Form from "./components/Form/Form";
 
-function App() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm({
-    defaultValues: {
-      randomText: "",
-      requiredText: "",
-    },
-  });
-
-  const [selectValue, setSelectValue] = useState(null);
-  const [checkDate, setCheckDate] = useState(true);
-
-  const randomText = watch("randomText");
-  const requiredText = watch("requiredText");
-
-  const handleSelectChange = (event) => setSelectValue(event.target.value);
-
-  const currentDate = new Date().toISOString().slice(0, 10);
-  const handleDatePick = (date, dateString) =>
-    setCheckDate(currentDate <= dateString);
-
-  const { Option } = Select;
-
+export default function App() {
   return (
     <div className="App">
       <h1>Test form</h1>
-      <form
-        onSubmit={handleSubmit((data) => {
-          console.log("data", data);
-        })}
-      >
-        <input
-          {...register("randomText", {
-            minLength: {
-              value: 5,
-              message: "Value doesn't meet the requirements",
-            },
-          })}
-          placeholder="Enter your text here"
-          style={{ width: "300px" }}
-        />
-
-        <input
-          {...register("requiredText", { required: "This is required" })}
-          placeholder="Field is required"
-          style={{ width: "300px" }}
-        />
-        <p>{errors.requiredText?.message}</p>
-
-        <select
-          {...register("dropdown")}
-          style={{ width: 250 }}
-          onChange={handleSelectChange}
-        >
-          <option value="A">A</option>
-          <option value="B">B</option>
-          <option value="C">C</option>
-        </select>
-
-        {selectValue === "C" && <p>{errors.randomText?.message}</p>}
-
-        <DatePicker {...register("datePicker")} onChange={handleDatePick} />
-        <Button
-          type="primary"
-          htmlType="submit"
-          style={
-            checkDate
-              ? {
-                  backgroundColor: "#08e672",
-                  borderColor: "#08e672",
-                }
-              : {
-                  backgroundColor: "#efc41a",
-                  borderColor: "#efc41a",
-                }
-          }
-        >
-          Submit
-        </Button>
-      </form>
+      <Form />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Form/Form.js
@@ -0,0 +1,115 @@
+import "antd/dist/antd.css";
+import { useForm, Controller } from "react-hook-form";
+import { Button, DatePicker, Select, Input, Dropdown } from "antd";
+import { useState } from "react";
+import React from "react";
+import styles from "./Form.module.css";
+
+const useDatePicker = () => {
+  const [checkDate, setCheckDate] = useState(true);
+  const currentDate = new Date().toISOString().slice(0, 10);
+  const handleDatePick = (date, dateString) =>
+    setCheckDate(currentDate <= dateString);
+
+  return { checkDate, handleDatePick };
+};
+
+const useSelect = () => {
+  const [selectValue, setSelectValue] = useState(null);
+  const handleSelectChange = (value) => setSelectValue(value);
+
+  return { selectValue, handleSelectChange };
+};
+
+export default function Form() {
+  const date = useDatePicker();
+  const select = useSelect();
+
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      randomText: "",
+      requiredText: "",
+    },
+  });
+
+  const { Option } = Select;
+
+  const onSubmit = (data) => {
+    data && date.checkDate && alert("Sukcess!");
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="randomText"
+        control={control}
+        rules={{ minLength: 5 }}
+        render={({ field }) => (
+          <Input
+            {...field}
+            style={{ width: "300px" }}
+            placeholder="Enter your text here"
+            className={styles.formElement}
+          />
+        )}
+      />
+
+      <Controller
+        name="requiredText"
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => (
+          <Input
+            {...field}
+            style={{ width: "300px" }}
+            placeholder="Enter your text here"
+            className={styles.formElement}
+          />
+        )}
+      />
+
+      {errors.requiredText && <p>To pole jest wymagane</p>}
+
+      <Controller
+        name="dropdown"
+        control={control}
+        render={({ field }) => (
+          <Select
+            {...field}
+            onChange={select.handleSelectChange}
+            style={{ width: 250 }}
+            placeholder="Choose an option"
+            className={styles.formElement}
+          >
+            <Option value="A">A</Option>
+            <Option value="B">B</Option>
+            <Option value="C">C</Option>
+          </Select>
+        )}
+      />
+
+      {select.selectValue === "C" && errors.randomText && (
+        <p>Nie spełnione kryteria</p>
+      )}
+
+      <DatePicker
+        {...register("datePicker")}
+        className={styles.formElement}
+        onChange={date.handleDatePick}
+      />
+
+      <Button
+        type="primary"
+        htmlType="submit"
+        className={date.checkDate ? styles.greenButton : styles.yellowButton}
+      >
+        Submit
+      </Button>
+    </form>
+  );
+}
